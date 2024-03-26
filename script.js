0606cba9bc04d587ee6cb25a5bfccdb72c5edec9
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-03-26 20:22:19 +0530

    Dom Elements-Form

---
+++ script.js
@@ -0,0 +1,16 @@
+function result(){
+let f_nm = document.getElementById("f_name").value;
+console.log(f_nm);
+let m_nm = document.getElementById("m_name").value;
+console.log(m_nm);
+let l_nm = document.getElementById("l_name").value;
+console.log(l_nm);
+let em = document.getElementById("email").value;
+console.log(em);
+let ad = document.getElementById("addr").value;
+console.log(ad);
+}
+
+
+
+
